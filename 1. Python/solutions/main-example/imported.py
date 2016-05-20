f32c29c5b65bd19640fac4c3fa8926a78f16bd91
Andrew Hedges <andrew@pdxcodeguild.com>
2016-05-20 15:04:34 -0700

    Example of use of __name__ to branch on execution context

---
+++ 1. Python/solutions/main-example/imported.py
@@ -0,0 +1,6 @@
+def do_something():
+    print('I did something.')
+
+if __name__ == '__main__':
+    print('I am about to do something.')
+    do_something()
---
+++ 1. Python/solutions/main-example/importer.py
@@ -0,0 +1,3 @@
+from imported import do_something
+
+do_something()

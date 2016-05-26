e6f2915c2b90d166b165e1ba0d773cbe8d11f81c
Andrew Hedges <andrew@pdxcodeguild.com>
2016-05-26 15:45:17 -0700

    Added tests to inheritance example

--- 1. Python/6/extend-a-class.md
+++
@@ -1,7 +0,0 @@
-# Exercise: Practice Inheritance
-
-## Objective
-
-Create new classes that inherit behaviors and attributes from existing classes.
-
-1. Save this file to your local code folder: inerhitance.py
--- 1. Python/solutions/inheritance.py
+++ 1. Python/solutions/inheritance.py
@@ -1,3 +1,94 @@
+__test__ = {
+    'Animal': """
+
+    >>> no_args_animal = Animal()
+    >>> no_args_animal.is_animal
+    True
+    >>> no_args_animal.is_vegetable
+    False
+    >>> no_args_animal.is_mineral
+    False
+    >>> no_args_animal.number_of_legs
+    0
+    >>> no_args_animal.what_i_say is None
+    True
+    >>> no_args_animal.speak()
+    Sorry, I don’t speak!
+
+    >>> animal_with_args = Animal(number_of_legs=3, what_i_say='shazbat')
+    >>> animal_with_args.is_animal
+    True
+    >>> animal_with_args.is_vegetable
+    False
+    >>> animal_with_args.is_mineral
+    False
+    >>> animal_with_args.number_of_legs
+    3
+    >>> animal_with_args.what_i_say is None
+    False
+    >>> animal_with_args.speak()
+    shazbat
+
+    """,
+    'Dog': """
+
+    >>> no_args_dog = Dog()
+    >>> no_args_dog.is_animal
+    True
+    >>> no_args_dog.number_of_legs
+    4
+    >>> no_args_dog.speak()
+    ruff
+    >>> no_args_dog.kind
+    'canine'
+    >>> no_args_dog.wag()
+    /wags tail
+
+    """,
+    'Pet': """
+
+    >>> no_args_pet = Pet()
+    >>> no_args_pet.name is None
+    True
+
+    >>> pet_with_arg = Pet(name='Puddles')
+    >>> pet_with_arg.name is None
+    False
+
+    """,
+    'PetDog': """
+
+    >>> no_args_pet_dog = PetDog()
+    >>> no_args_pet_dog.is_animal
+    True
+    >>> no_args_pet_dog.number_of_legs
+    4
+    >>> no_args_pet_dog.speak()
+    ruff
+    >>> no_args_pet_dog.kind
+    'canine'
+    >>> no_args_pet_dog.wag()
+    /wags tail
+    >>> no_args_pet_dog.name is None
+    True
+
+    >>> pet_dog_with_arg = PetDog(name='Fido')
+    >>> pet_dog_with_arg.is_animal
+    True
+    >>> pet_dog_with_arg.number_of_legs
+    4
+    >>> pet_dog_with_arg.speak()
+    ruff
+    >>> pet_dog_with_arg.kind
+    'canine'
+    >>> pet_dog_with_arg.wag()
+    /wags tail
+    >>> pet_dog_with_arg.name is None
+    False
+
+    """
+}
+
 class Animal:
     """Blueprint for a generic animal"""
 
@@ -15,6 +106,7 @@ class Animal:
         else:
             print(self.what_i_say)
 
+
 class Dog(Animal):
     """Blueprint for a dog (inherits from Animal)"""
 
@@ -22,11 +114,12 @@ class Dog(Animal):
 
     def __init__(self):
         super().__init__()
-        self.number_of_legs=4
-        self.what_i_say='ruff'
+        self.number_of_legs = 4
+        self.what_i_say = 'ruff'
 
     def wag(self):
         print('/wags tail')
+
 
 class Pet:
     """Blueprint for a generic pet"""
@@ -34,9 +127,15 @@ class Pet:
     def __init__(self, name=None):
         self.name = name
 
+
 class PetDog(Dog, Pet):
     """Blueprint for a dog that is also a pet"""
 
     def __init__(self, name=None):
         super().__init__()
         self.name = name
+
+if __name__ == '__main__':
+    from doctest import testmod
+
+    testmod()  # Run our unit tests when the script is run with the -v option

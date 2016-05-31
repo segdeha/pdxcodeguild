4a27b6d97b99e7fcb7ce029485d387337c2df794
Andrew Hedges <andrew@pdxcodeguild.com>
2016-05-31 13:41:32 -0700

    Better whitespace

--- 1. Python/solutions/dice.py
+++ 1. Python/solutions/dice.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 """PDX Code Guild Curriculum. Dice example."""
 
+
 __author__ = "Christopher Jones"
 __copyright__ = "Copyright 2016, PDX Code Guild"
 __version__ = "0.1"
 
+
 from random import randint
+
 
 def dice(number_of_dice, number_of_sides):
     for die in range(1, number_of_dice + 1):

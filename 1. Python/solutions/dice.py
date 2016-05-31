5cccc4f8e653db3e36298a50f08b15c2d6119444
Andrew Hedges <andrew@pdxcodeguild.com>
2016-05-31 14:00:03 -0700

    Changed method name to roll_dice

--- 1. Python/solutions/dice.py
+++ 1. Python/solutions/dice.py
@@ -8,7 +8,7 @@ __version__ = "0.1"
 from random import randint
 
 
-def dice(number_of_dice, number_of_sides):
+def roll_dice(number_of_dice, number_of_sides):
     for die in range(1, number_of_dice + 1):
         random_integer = randint(1, number_of_sides)
         print("Die number " + str(die) + ": " + str(random_integer))
@@ -16,4 +16,4 @@ def dice(number_of_dice, number_of_sides):
 dice_number = int(input("How many dice would you like to roll? "))
 dice_sides = int(input("How many sides should each die have? "))
 
-dice(dice_number, dice_sides)
+roll_dice(dice_number, dice_sides)

5d729b8dcdf13999919ff68904de50039f9e0b3d
Andrew Hedges <andrew@pdxcodeguild.com>
2016-06-21 14:12:24 -0700

    Added params to app.js

--- 3. JavaScript/solutions/modular_javascript/js/app.js
+++ 3. JavaScript/solutions/modular_javascript/js/app.js
@@ -1,20 +1,31 @@
 requirejs(
-    ['./jquery', './semantic', './ingredients-updater', './total-updater', './validator'],
+    [
+        './jquery',
+        './semantic',
+        './ingredients-updater',
+        './total-updater',
+        './validator'
+    ],
     function ($, semantic, updateIngredients, updateTotal, validate) {
 
     // initialize semantic-ui checkboxes and radio buttons
     $('.ui.checkbox').checkbox()
 
     var form = document.querySelector('form');
+    var total_output = document.getElementById('total_cost');
+    var ingredients_output = document.getElementById('ingredients');
+    var costs = { 'extra-ingredients': 0.5, 'delivery': 5 };
+    var ingredients = ['tortilla', 'meat', 'included-ingredients', 'extra-ingredients'];
+    var requiredFields = ['name', 'credit-card', 'ccv', 'zip', 'terms'];
 
     form.addEventListener('change', function (evt) {
-        updateTotal();
-        updateIngredients();
+        updateTotal(form, total_output, costs);
+        updateIngredients(form, ingredients_output, ingredients);
     });
 
     form.addEventListener('submit', function (evt) {
         // prevent the form from submitting when the user hits the submit button
         evt.preventDefault();
-        validate();
+        validate(form, requiredFields);
     });
 });
